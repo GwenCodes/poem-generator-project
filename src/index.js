7a76e4f9321e0e4198acb084364f290c8dbe465d
GwenCodes <essexdesigns43.0@gmail.com>
2025-06-12 17:02:36 -0400

    Refined context variable, created generating animation
    Better user experience with generating animation

--- src/index.js
+++ src/index.js
@@ -7,20 +7,25 @@ function displayPoem(response) {
     cursor: "",
   });
 }
+
 function generatePoem(event) {
+  //step 3 display the generated poem
   event.preventDefault(); // step 4
-  //step 3
-  //display the generated poem
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "61dfa07e8o8462teba3fadad30e70d77"; //step 6; build api url ad api key
   //step 7; make a call to the api url
   let context =
-    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
-  let prompt = `User instructions: generate a poem about ${instructionsInput.value}`;
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    "You are a romantic poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(displayPoem); //step 7
+  let poemElement = document.querySelector("#poem"); //step 8
+  poemElement.classList.remove("hidden"); // step 9
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`; //step 10; added blinking animation
+
+  axios.get(apiURL).then(displayPoem);
 }
+
 let poemFormElement = document.querySelector("#poem-generator-form"); //step 1
 poemFormElement.addEventListener("submit", generatePoem); //step 2
